6aa83300d03e098e074996670965a65e8e2cf423
monjara <yaginumee@gmail.com>
2024-05-28 09:36:51 +0900

    style

--- src/content/components/tree.jsx
+++ src/content/components/tree.jsx
@@ -3,16 +3,22 @@ import { useState } from 'react'
 import FolderIcon from './folder-icon'
 import FolderOpenIcon from './folder-open-icon'
 
-const LinkRow = ({ item }) => {
+const LinkRow = ({ item, depth }) => {
   return (
-    <a href={item.url} className='link-row'>
+    <a
+      href={item.url}
+      className='link-row'
+      style={{
+        paddingLeft: depth,
+      }}
+    >
       <img src={getFavicon(item.url)} alt='' style={{ width: 16 }} />
       <span>{item.title}</span>
     </a>
   )
 }
 
-const FolderRow = ({ item }) => {
+const FolderRow = ({ item, depth }) => {
   const [open, setOpen] = useState(false)
   const toggle = () => {
     setOpen((old) => !old)
@@ -20,19 +26,29 @@ const FolderRow = ({ item }) => {
 
   return (
     <>
-      <div className='folder-row' onClick={toggle} onKeyUp={toggle}>
+      <div
+        className='folder-row'
+        onClick={toggle}
+        onKeyUp={toggle}
+        style={{
+          paddingLeft: depth,
+        }}
+      >
         {open ? <FolderOpenIcon /> : <FolderIcon />}
         <span>{item.title}</span>
       </div>
       {open &&
-        item.children.map((child) => <Tree key={child.id} item={child} />)}
+        item.children.map((child) => (
+          <Tree key={child.id} item={child} depth={depth} />
+        ))}
     </>
   )
 }
 
-export default function Tree({ item }) {
+export default function Tree({ item, depth = 0 }) {
+  const childDepth = depth + 10
   if (!item?.children) {
-    return <LinkRow item={item} />
+    return <LinkRow item={item} depth={childDepth} />
   }
-  return <FolderRow item={item} />
+  return <FolderRow item={item} depth={childDepth} />
 }
